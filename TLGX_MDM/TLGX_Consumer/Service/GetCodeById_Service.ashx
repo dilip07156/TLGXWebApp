830b95a494be474d85ea304f5eb6e09168c83539
Ajay Kumar <ajay.kumar@coxandkings.com>
2017-07-04 17:40:09 +0530

    Added code to fetch state name and state code

---
+++ TLGX_MDM/TLGX_Consumer/Service/GetCodeById_Service.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="GetCodeById_Service.ashx.cs" Class="TLGX_Consumer.Service.GetCodeById_Service" %>
---
+++ TLGX_MDM/TLGX_Consumer/Service/GetCodeById_Service.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Script.Serialization;
+using TLGX_Consumer.Controller;
+
+namespace TLGX_Consumer.Service
+{
+    /// <summary>
+    /// Summary description for HotelNameAutoComplete
+    /// </summary>
+    public class GetCodeById_Service : IHttpHandler
+    {
+        MasterDataSVCs _obj = new MasterDataSVCs();
+        public void ProcessRequest(HttpContext context)
+        {
+            var PrefixText = context.Request.QueryString["state_id"];
+            var res = _obj.GetCodeById("state", PrefixText);
+            context.Response.Write(new JavaScriptSerializer().Serialize(res));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
