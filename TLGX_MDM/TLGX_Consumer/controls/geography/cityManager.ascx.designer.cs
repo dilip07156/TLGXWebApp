2b9f98389309d3c17cd29e03575fda326154ac8a
Pradeep Gond <pradeep.gond@coxandkings.com>
2017-07-13 12:12:33 +0530

    City Manager.

--- TLGX_MDM/TLGX_Consumer/controls/geography/cityManager.ascx.designer.cs
+++ TLGX_MDM/TLGX_Consumer/controls/geography/cityManager.ascx.designer.cs
@@ -19,7 +19,7 @@ namespace TLGX_Consumer.controls.geography {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel panSearchConditions;
+        protected global::System.Web.UI.UpdatePanel panSearchConditions;
         
         /// <summary>
         /// vlsSumm control.
@@ -56,6 +56,15 @@ namespace TLGX_Consumer.controls.geography {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton btnGetCities;
+        
+        /// <summary>
+        /// btnNewCity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnNewCity;
         
         /// <summary>
         /// cityResult control.
@@ -191,5 +200,41 @@ namespace TLGX_Consumer.controls.geography {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.FormView frmCityAreaLocation;
+        
+        /// <summary>
+        /// updpnlCity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel updpnlCity;
+        
+        /// <summary>
+        /// vldsumCity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary vldsumCity;
+        
+        /// <summary>
+        /// dvmsgCityAlert control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvmsgCityAlert;
+        
+        /// <summary>
+        /// frmvwCity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FormView frmvwCity;
     }
 }
