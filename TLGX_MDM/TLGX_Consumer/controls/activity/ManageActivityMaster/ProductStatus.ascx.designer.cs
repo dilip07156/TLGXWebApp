a5529063d9bf8010015d7a5d5a5102b154bce8de
Pradeep Gond <pradeep.gond@coxandkings.com>
2017-09-25 14:22:42 +0530

    Activity Master 'Product Status' added

--- TLGX_MDM/TLGX_Consumer/controls/activity/ManageActivityMaster/ProductStatus.ascx.designer.cs
+++ TLGX_MDM/TLGX_Consumer/controls/activity/ManageActivityMaster/ProductStatus.ascx.designer.cs
@@ -13,13 +13,13 @@ namespace TLGX_Consumer.controls.activity.ManageActivityMaster {
     public partial class ProductStatus {
         
         /// <summary>
-        /// updNewActivity control.
+        /// updPanStatus control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.UpdatePanel updNewActivity;
+        protected global::System.Web.UI.UpdatePanel updPanStatus;
         
         /// <summary>
         /// dvMsg control.
@@ -31,84 +31,21 @@ namespace TLGX_Consumer.controls.activity.ManageActivityMaster {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvMsg;
         
         /// <summary>
-        /// ddlMarket control.
+        /// frmActivityStatus control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlMarket;
+        protected global::System.Web.UI.WebControls.FormView frmActivityStatus;
         
         /// <summary>
-        /// ddlStatus control.
+        /// grdStatusList control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlStatus;
-        
-        /// <summary>
-        /// txtFrom control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtFrom;
-        
-        /// <summary>
-        /// calFromDate control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::AjaxControlToolkit.CalendarExtender calFromDate;
-        
-        /// <summary>
-        /// axfte_txtFrom control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::AjaxControlToolkit.FilteredTextBoxExtender axfte_txtFrom;
-        
-        /// <summary>
-        /// txtReason control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtReason;
-        
-        /// <summary>
-        /// btnNewActivity control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnNewActivity;
-        
-        /// <summary>
-        /// dvGrid control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvGrid;
-        
-        /// <summary>
-        /// grdSearchResults control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView grdSearchResults;
+        protected global::System.Web.UI.WebControls.GridView grdStatusList;
     }
 }
