636f9be71732261bbf6f1e3ff1440b46cbfed400
yogita bhor <yogita.bhor@coxandkings.com>
2017-10-09 14:27:56 +0530

    gridview changes

--- TLGX_MDM/TLGX_Consumer/controls/activity/ManageActivityFlavours/ActivityMedia.ascx.designer.cs
+++ TLGX_MDM/TLGX_Consumer/controls/activity/ManageActivityFlavours/ActivityMedia.ascx.designer.cs
@@ -22,15 +22,6 @@ namespace TLGX_Consumer.controls.activity.ManageActivityFlavours {
         protected global::System.Web.UI.UpdatePanel updMedia;
         
         /// <summary>
-        /// btnNewUpload control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnNewUpload;
-        
-        /// <summary>
         /// lblTotalRecords control.
         /// </summary>
         /// <remarks>
