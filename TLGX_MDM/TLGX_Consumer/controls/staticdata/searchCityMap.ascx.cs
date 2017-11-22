0773fd70d281e4e06ab040ab70987f6edc781127
Ajay Kumar <ajay.kumar@coxandkings.com>
2017-11-22 16:11:41 +0530

    modified code to set city code in modal

--- TLGX_MDM/TLGX_Consumer/controls/staticdata/searchCityMap.ascx.cs
+++ TLGX_MDM/TLGX_Consumer/controls/staticdata/searchCityMap.ascx.cs
@@ -425,6 +425,10 @@ namespace TLGX_Consumer.controls.staticdata
                                     foreach (MDMSVC.DC_City_Master_DDL resc in querynamed)
                                     {
                                         ddlSystemCityName.SelectedIndex = ddlSystemCityName.Items.IndexOf(ddlSystemCityName.Items.FindByValue(resc.City_Id.ToString()));
+                                        if (ddlSystemCityName.SelectedIndex > 0)
+                                        {
+                                            txtSystemCityCode.Text = resc.Code;
+                                        }
                                     }
                                 }
                                 if (ddlSystemCityName.SelectedIndex == 0)
@@ -435,6 +439,11 @@ namespace TLGX_Consumer.controls.staticdata
                                         foreach (MDMSVC.DC_City_Master_DDL resc in queryname)
                                         {
                                             ddlSystemCityName.SelectedIndex = ddlSystemCityName.Items.IndexOf(ddlSystemCityName.Items.FindByValue(resc.City_Id.ToString()));
+                                            if(ddlSystemCityName.SelectedIndex > 0)
+                                            {
+                                                txtSystemCityCode.Text = resc.Code;
+                                            }
+                                            
                                         }
                                     }
                                 }
@@ -449,6 +458,10 @@ namespace TLGX_Consumer.controls.staticdata
                                         foreach (MDMSVC.DC_City_Master_DDL resc in queryname)
                                         {
                                             ddlSystemCityName.SelectedIndex = ddlSystemCityName.Items.IndexOf(ddlSystemCityName.Items.FindByValue(resc.City_Id.ToString()));
+                                            if (ddlSystemCityName.SelectedIndex > 0)
+                                            {
+                                                txtSystemCityCode.Text = resc.Code;
+                                            }
                                         }
                                     }
                                 }
@@ -460,6 +473,10 @@ namespace TLGX_Consumer.controls.staticdata
                                         foreach (MDMSVC.DC_City_Master_DDL resc in queryname)
                                         {
                                             ddlSystemCityName.SelectedIndex = ddlSystemCityName.Items.IndexOf(ddlSystemCityName.Items.FindByValue(resc.City_Id.ToString()));
+                                            if (ddlSystemCityName.SelectedIndex > 0)
+                                            {
+                                                txtSystemCityCode.Text = resc.Code;
+                                            }
                                         }
                                     }
                                 }
