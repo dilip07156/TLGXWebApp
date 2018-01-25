3fe30a87c3d264fb58d8524e6d7731ecbb062f9f
yogita bhor <yogita.bhor@coxandkings.com>
2018-01-25 13:58:28 +0530

    Dashboard date ammendments

--- TLGX_MDM/TLGX_Consumer/staticdata/velocityReport.aspx.designer.cs
+++ TLGX_MDM/TLGX_Consumer/staticdata/velocityReport.aspx.designer.cs
@@ -40,6 +40,24 @@ namespace TLGX_Consumer.staticdata {
         protected global::System.Web.UI.WebControls.DropDownList ddlSupplierName;
         
         /// <summary>
+        /// ddlDateOptions control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlDateOptions;
+        
+        /// <summary>
+        /// dvSpecificDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvSpecificDate;
+        
+        /// <summary>
         /// txtFrom control.
         /// </summary>
         /// <remarks>
