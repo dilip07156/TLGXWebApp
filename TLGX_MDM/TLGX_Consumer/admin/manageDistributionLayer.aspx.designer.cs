4a3fb24f27b0646447e3b569d03a639fc9bb9179
Radha.Dixit <Radha.Dixit@coxandkings.com>
2018-05-07 10:55:01 +0530

    Committed changes for Supplier Static hotel

--- TLGX_MDM/TLGX_Consumer/admin/manageDistributionLayer.aspx.designer.cs
+++ TLGX_MDM/TLGX_Consumer/admin/manageDistributionLayer.aspx.designer.cs
@@ -191,5 +191,14 @@ namespace TLGX_Consumer.admin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnRefreshSupplyMaster;
+        
+        /// <summary>
+        /// grdSupplierEntity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdSupplierEntity;
     }
 }
